f594aed9453cc801be50090e05824f9fe7958591
n9br <andre.neugebauer@gmail.com>
2023-02-09 19:42:34 +0100

    clean filebackend stuff

--- glitter-server.js
+++ glitter-server.js
@@ -1,17 +1,17 @@
 
-const fs = require("fs");
+/**
+ * Const.
+ * 
+ */
+
 const cors = require("cors");
 const { Client } = require('pg');
 
-// const bodyParser = require('body-parser');
-// const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
 const express = require("express");     
 const { request } = require("http");
-// const { text } = require("body-parser");
+
 const app = express();
 const port = 4000;
-
 
 const client = new Client({
   user: "postgres", 
@@ -20,19 +20,21 @@ const client = new Client({
 });
 
 client.connect();   // hier richtig
-  // console.log('Success connect');
 
-// client.query('SELECT $1::text as message', ['Hello world from postgres!'], (err, res) => {
-//   console.log(err ? err.stack : res.rows[0].message) // Hello World!
-//   client.end()
-// })
-
-// const glitsFile = "./glits.json";
+/**
+ * Middleware.
+ */
 
 app.use(cors({
   origin: '*'
 }));
 app.use(express.json());
+
+/**#
+ * Class.
+ * 
+ */
+
 
 /**
  *avatarId: number;
@@ -57,16 +59,6 @@ class Glit {
   }
 }
 
-// function readGlitsFromFile() {
-//   try {
-//     const filedata = fs.readFileSync(glitsFile).toString();
-//     const jsonglits = JSON.parse(filedata);
-//     return jsonglits;
-//   } catch (e) {
-//     return []
-//   }
-// }
-
 /**
  * @params : Standard
  */
@@ -74,7 +66,6 @@ function getGlitsFromDB (req, response) {
   client.query("SELECT * FROM glits ORDER BY datetime DESC", (err, result) => {
     console.log(result.rows);
     response.send(result.rows);
-    // response.send(result.rows.reverse());
   })
   // return(result.rows);
 }
@@ -82,47 +73,25 @@ function getGlitsFromDB (req, response) {
 // Glits Get
 app.get('/glits',getGlitsFromDB)
 
-// app.get('/glits', (req, res) => {
-//     // res.send(readGlitsFromFile().reverse())
-//     res.send(getGlitsFromDB());
-//   })
 
-// simpel
-// app.get('/glits', (req,res) => {
-//     client.query('SELECT * FROM glits', (err, results) => {
-//       res.send(results.rows);
-//     })
+// Glits Post
+app.post('/glits',postGlitsToDB) 
 
-//   }
-// )
+function postGlitsToDB(request, response) {
+  const glit = new Glit(request.body);
+  console.log(glit);
 
-
-  // Glits Post
-  app.post('/glits',postGlitsToDB) 
-
-  function postGlitsToDB(request, response) {
-    const glit = new Glit(request.body);
-    console.log(glit);
-
-    const queryString = "INSERT INTO glits (avatarId, \"user\", text, datetime) VALUES ($1, $2, $3, $4)"
-    // console.log(client.query(queryString, [glit.avatarId, glit.user, glit.text, glit.datetime]));
-    client.query(queryString, [glit.avatarId, glit.user, glit.text, glit.datetime], (err, result) => {
-      if (err) {
-        response.status(400);
-      }
-      response.status(201);
-      response.send(glit);
-      }
-    )
-  }
-// app.post('/glits', (req, res) => {
-//   // console.log(req.body);    
-//     const glits = readGlitsFromFile();
-//     const glit = new Glit(req.body);          // from payload
-//     glits.push(glit);
-//     fs.writeFileSync(glitsFile,JSON.stringify(glits));
-//     res.status(201).send();                         // set statuscode 201 and send
-// })                                 
+  const queryString = "INSERT INTO glits (avatarId, \"user\", text, datetime) VALUES ($1, $2, $3, $4)"
+  // console.log(client.query(queryString, [glit.avatarId, glit.user, glit.text, glit.datetime]));
+  client.query(queryString, [glit.avatarId, glit.user, glit.text, glit.datetime], (err, result) => {
+    if (err) {
+      response.status(400);
+    }
+    response.status(201);
+    response.send(glit);
+    }
+  )
+}                            
 
 // Hello World
 app.get('/', (req, res) => {
